9d468fe498e67a6f858434f78940a40c7417787d
Pranav <pranav.kalpvaig.3212@gmail.com>
2025-08-02 23:41:23 +0530

    Completed upto ep 10

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,23 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Arr;
 
 class Job extends Model {
-    public static function all(): array
-    {
-       //
+    use HasFactory;
+   protected $table = 'job_listings';
 
-    }
-    public static function find (int $id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] = $id);
-        
-        if (! $job){
-            abort(404);
-        }
-        return $job;
-    }
+   protected $fillable = ['title', 'salary'];
 }
 
 // return Arr::first(static::all(), fn($job) => $job['id'] = $id);
