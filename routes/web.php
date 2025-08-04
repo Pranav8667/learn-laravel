251a57a77136dfeceb7259fc4303e467804aa3d9
Pranav <pranav.kalpvaig.3212@gmail.com>
2025-08-04 15:46:09 +0530

    half complete of ep16

--- routes/web.php
+++ routes/web.php
@@ -12,24 +12,38 @@ Route::get('/', function () {
     return view('home');
 });
 
+
+//All jobs showing route
+
 Route::get('/jobs', function (){
     // $jobs = Job::with('employer')->get();
     // $jobs = Job::with('employer')->simplePaginate(3); //ep14
     $jobs = Job::with('employer')->paginate(3);   //ep 14
     // $jobs = Job::with('employer')->cursorPaginate(3);   //ep14
     // $jobs = Job::all();
-    return view('jobs', [
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
 });
 
+//Route to show a new page to create a jobs
+
+Route::get('/jobs/create', function (){
+    return view('jobs.create');
+    // dd('hello there');
+});
+
+//Single showing job route
 Route::get('/jobs/{id}', function ($id){
    
     // $job = Arr::first(Job::all(), fn($job) => $job['id'] = $id);
     $job = Job::find($id);
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
 });
 
+Route::post('/jobs', function () {
+
+});
 
 Route::get('/contact', function () {
     return view('contact');
