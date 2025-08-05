9c037e243dff62f87b44136c7e25021fdfb43469
Pranav <pranav.kalpvaig.3212@gmail.com>
2025-08-05 22:57:28 +0530

    completed upto ep 19

--- routes/web.php
+++ routes/web.php
@@ -1,108 +1,32 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
-// Day 1
-// Ep 6 view and route file jobs
+
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
+
+Route::resource('jobs', JobController::class);
 
 
 
-Route::get('/', function () {
-    return view('home');
-});
+
+// Route::controller(JobController::class)->group(function () {
+//     Route::get('/jobs', [JobController::class, 'index']);
+//     Route::get('/jobs/create', [JobController::class, 'create']);
+//     Route::get('/jobs/{job}', [JobController::class, 'show']);
+//     Route::post('/jobs', [JobController::class, 'store']);
+//     Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
+//     Route::patch('/jobs/{job}', [JobController::class, 'update']);
+//     Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
+// });
 
 
-//All jobs showing route or index
-
-Route::get('/jobs', function (){
-    // $jobs = Job::with('employer')->get();
-    // $jobs = Job::with('employer')->simplePaginate(3); //ep14
-    $jobs = Job::with('employer')->latest()->paginate(3);   //ep 14
-    // $jobs = Job::with('employer')->cursorPaginate(3);   //ep14
-    // $jobs = Job::all();
-    return view('jobs.index', [
-        'jobs' => $jobs
-    ]);
-});
-
-//Route to show a new page to create a jobs
-
-Route::get('/jobs/create', function (){
-    return view('jobs.create');
-    // dd('hello there');
-});
-
-//Single showing job route
-Route::get('/jobs/{id}', function ($id){
-   
-    // $job = Arr::first(Job::all(), fn($job) => $job['id'] = $id);
-    $job = Job::find($id);
-    return view('jobs.show', ['job' => $job]);
-});
-
-
-//Store
-Route::post('/jobs', function () {
-    //Validation
-
-    request()->validate([
-        'title' => ['required', 'min:3'], 
-        'salary' => ['required']
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1
-    ]);
-
-    return redirect('/jobs');
-    // dd(request()->all());
-    // dd(request('title'));
-    // dd('hello from the post request');
-});
-
-// Edit a job
-Route::get('/jobs/{id}/edit', function ($id){
-    $job = Job::find($id);
-    return view('jobs.edit', ['job' => $job]);
-});
-
-//update
-
-Route::patch('/jobs/{id}', function ($id){
-    //validate
-
-      request()->validate([
-        'title' => ['required', 'min:3'], 
-        'salary' => ['required']
-    ]);
-    //authorize (on hold..)
-    $job = Job::findOrFail($id);
-
-   $job->update([
-    'title' => request('title'),
-    'salary' => request('salary')
-   ]);
-   
-    return redirect('/jobs/'. $job->id);
-});
-
-//Destroy
-
-Route::delete('/jobs/{id}', function ($id){
-    //Authorize (On Hold..)
-
-
-    //Delete the job
-    Job::findOrFail($id)->delete();
-
-    //redirect to page 
-    return redirect('/jobs');
-});
-
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+// Route::get('/contact', function () {
+//     return view('contact');
+// });
